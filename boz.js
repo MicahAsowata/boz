38e86f7d8c9ea7df92972e0f720d5ff8168a652e
Micah Asowata <andrewgary963@gmail.com>
2023-05-03 01:58:11 +0100

    Smoothen out the code

--- boz.js
+++ boz.js
@@ -1,59 +1,64 @@
 import { input, select } from "@inquirer/prompts";
+console.log("Welcome to Boz");
 const game = {};
 const player = {};
-game.name = await input({ message: "Enter the name of the game" });
+game.name = await input({ message: "Enter the name of your game" });
 game.players = [];
 for (;;) {
   const choice = await select({
     message: "What do you want to do",
     choices: [
       {
-        name: "n",
-        value: "n",
-        description: "add a new player to the leader board",
+        name: "create",
+        value: "create",
+        description: "Create a new player",
       },
       {
-        name: "u",
-        value: "u",
-        description: "update the score of a current player",
+        name: "update",
+        value: "update",
+        description: "Update a current player",
       },
       {
-        name: "d",
-        value: "d",
-        description: "remove a player from the leader board",
+        name: "delete",
+        value: "delete",
+        description: "Delete a player",
       },
     ],
   });
   switch (choice) {
-    case "n":
-      console.log("Creating a new player");
+    case "create":
+      console.log("Creating a player");
       player.id = game.players.length + 1;
-      player.name = await input({ message: "What is the player's name" });
+      player.name = await input({ message: "Enter player's name" });
       player.score = parseInt(
-        await input({ message: `What did ${player.name} score` })
+        await input({ message: `Enter ${player.name}'s score` })
       );
       game.players.push(player);
       break;
-    case "u":
-      console.log("Updating a player score");
-      const playerId = parseInt(await input({ message: "Enter player id" }));
-      if (playerId <= game.players.length) {
-        game.players[playerId - 1].score = parseInt(
+    case "update":
+      console.log("Update a player");
+      const playerToUpdateID = parseInt(
+        await input({ message: "Enter player id" })
+      );
+      if (playerToUpdateID <= game.players.length) {
+        game.players[playerToUpdateID - 1].score = parseInt(
           await input({ message: "Enter new score" })
         );
         console.log(game.players);
       } else {
-        console.log("That player does not exist");
+        console.log("Player does not exist");
       }
       break;
-    case "d":
-      console.log("removing a player");
-      const playerIndex = parseInt(await input({ message: "Enter player id" }));
-      if (playerIndex <= game.players.length) {
-        game.players.splice(playerIndex - 1, player + 1);
-        console.log(game.players[playerIndex]);
+    case "delete":
+      console.log("Delete a player");
+      const playerToDeleteID = parseInt(
+        await input({ message: "Enter player id" })
+      );
+      if (playerToDeleteID <= game.players.length) {
+        game.players.splice(playerToDeleteID - 1, playerToDeleteID + 1);
+        console.log(game.players[playerToDeleteID]);
       } else {
-        console.log("That player does not exist");
+        console.log("Player does not exist");
       }
       break;
     default:
